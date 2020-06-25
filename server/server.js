2fc261c91460808048d7542cefc888ef15b50648
Josiman <jmgomez@moviltech.mx>
2020-06-24 19:22:08 -0500

    express server ready with react app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+
 
 const resourceFolder = path.join(__dirname,'../public')
 
