279fc394950f36378daaa936d612217bb53d008d
Josiman <jmgomez@moviltech.mx>
2020-06-25 17:37:48 -0500

    formato de moneda y fecha

--- src/components/ArticulosListItem.js
+++ src/components/ArticulosListItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ArticulosListItem = ( { id, titulo, precio, creadoEl } ) => (
     <div>
-    <Link to={'/edit/'+ id}>{titulo}</Link>/{precio} / {creadoEl}
+    <Link to={`/edit/${id}`} >
+        <h3>{titulo}</h3> 
+    </Link>
+    <p>
+        { numeral(precio).format('0,0.00') } - { moment(creadoEl).format('DD-MMM-YY') }
+        
+    </p>
     </div>
 );
 
-
 export default ArticulosListItem;
+
